e244521063b014d3f17b1327b80f00eccdba88c2
Amanda Fernandes <aferanda@gmail.com>
2021-12-09 23:09:50 -0300

    Realiza chamada da API ao renderizar a página

--- src/context/FoodRecipesProvider.js
+++ src/context/FoodRecipesProvider.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { FoodRecipesContext } from './RecipesContext';
+import { foodsAPI } from '../services/resquestAPI';
 
 function FoodRecipesProvider({ children }) {
+  const [categoriesList, setCategoriesList] = useState([]);
+  const [mealsRecipes, setMealsRecipes] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await foodsAPI.get('/list.php?c=list');
+      setCategoriesList(data);
+      console.log(data);
+    })();
+    (async () => {
+      const { data } = await foodsAPI.get('/search.php?s=');
+      setMealsRecipes(data);
+      console.log(data);
+    })();
+  }, []);
+
+  const context = {
+    categoriesList,
+    mealsRecipes,
+  };
+
   return (
-    <FoodRecipesContext.Provider>
+    <FoodRecipesContext.Provider value={ context }>
       {children}
     </FoodRecipesContext.Provider>
   );
