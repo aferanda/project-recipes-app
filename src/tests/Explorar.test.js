fff11242aa4cb768e351f9bfcbf665de8871b04b
Rafael Colombo <rcolombodev@gmail.com>
2021-12-12 07:51:20 -0300

    Refatora teste do Explorar retirando provider

--- src/tests/Explorar.test.js
+++ src/tests/Explorar.test.js
@@ -7,46 +7,26 @@ import HeaderProvider from '../context/HeaderProvider';
 
 describe('Testing Explore Page', () => {
   it('should be render a button with "Comidas"', () => {
-    renderWithRouter(
-      <HeaderProvider>
-        <Explore />
-        ,
-      </HeaderProvider>,
-    );
+    renderWithRouter(<Explore />);
     const foodButton = screen.getByText(/Comidas/i);
     expect(foodButton).toBeDefined();
   });
 
   it('should be render a button with "Bebidas"', () => {
-    renderWithRouter(
-      <HeaderProvider>
-        <Explore />
-        ,
-      </HeaderProvider>,
-    );
+    renderWithRouter(<Explore />);
     const drinkButton = screen.getByText(/Bebidas/i);
     expect(drinkButton).toBeDefined();
   });
 
   it('should be redirect to explore food page', () => {
-    const { history } = renderWithRouter(
-      <HeaderProvider>
-        <Explore />
-        ,
-      </HeaderProvider>,
-    );
+    const { history } = renderWithRouter(<Explore />);
     const foodButton = screen.getByText(/Comidas/i);
     userEvent.click(foodButton);
     const { pathname } = history.location;
     expect(pathname).toBe('/explorar/comidas');
   });
   it('should be redirect to explore drink page', () => {
-    const { history } = renderWithRouter(
-      <HeaderProvider>
-        <Explore />
-        ,
-      </HeaderProvider>,
-    );
+    const { history } = renderWithRouter(<Explore />);
     const drinkButton = screen.getByText(/Bebidas/i);
     userEvent.click(drinkButton);
     const { pathname } = history.location;
