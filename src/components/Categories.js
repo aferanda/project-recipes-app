f9e3ba8db577a24ec13de610073d1d48fbb6c3e1
Rafael Colombo <rcolombodev@gmail.com>
2022-01-04 17:00:53 -0300

    Implementação do filtro de categoria

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,13 +1,18 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useContext } from 'react';
+import { FoodRecipesContext } from '../context/RecipesContext';
 
 function Categories(props) {
   const { categoryName } = props;
+  const { setSelectedCategory } = useContext(FoodRecipesContext);
 
   return (
     <button
       type="button"
+      onClick={ ({ target: { name } }) => setSelectedCategory(name) }
       data-testid={ `${categoryName}-category-filter` }
+      name={ categoryName }
+
     >
       { categoryName.split(' ')[0].split('/')[0] }
     </button>
