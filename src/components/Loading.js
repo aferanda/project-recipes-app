4d0ffada4dbc4ec0587a56b696534b76bff57b99
Amanda Fernandes <aferanda@gmail.com>
2022-01-17 10:14:39 -0300

    Cria componente "loading"

---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/loading.css';
+
+function Loading() {
+  return (
+    <div className="ctn-loader">
+      <div className="loader" />
+    </div>
+  );
+}
+
+export default Loading;
