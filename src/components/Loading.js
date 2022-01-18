4180755af246bdb25732d6275843cfbde3ccafd3
Amanda Fernandes <aferanda@gmail.com>
2022-01-17 23:06:48 -0300

    Modifica layout do loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -12,7 +12,12 @@ function Loading() {
 
   return (
     <div className="ctn-loader">
-      <div className="loader" />
+      <div className="lds-ellipsis">
+        <div />
+        <div />
+        <div />
+        <div />
+      </div>
     </div>
   );
 }
